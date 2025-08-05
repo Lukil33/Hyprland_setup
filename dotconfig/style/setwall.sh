9e0fd02aa8aee145efb401fa8407a30640e65004
Lukil <prigioneluca33@gmail.com>
2025-08-05 14:35:13 +0200

    fix: :bug: migliorato lo stile di rofi

--- dotconfig/style/setwall.sh
+++ dotconfig/style/setwall.sh
@@ -3,28 +3,37 @@
 # Numero di immagini fornite come argomento
 IMAGE_COUNT=$#
 
-if [ "$IMAGE_COUNT" -lt 1 || "$IMAGE_COUNT" -gt 2 ]; then
+if [[ "$IMAGE_COUNT" -lt 1 || "$IMAGE_COUNT" -gt 2 ]]; then
+  echo "Usage: $0 image1 [image2]"
   exit 1;
 fi
 
 # Modifica le impostazioni dello sfondo su hyprpaper.conf
 if [ "$IMAGE_COUNT" -eq 1 ]; then
-  update_hyprpaper.sh "$1"
+  $HOME/.config/style/update_hyprpaper.sh "$1"
 elif [ "$IMAGE_COUNT" -eq 2 ]; then
-  update_hyprpaper.sh "$1" "$2"
+  $HOME/.config/style/update_hyprpaper.sh "$1" "$2"
 fi
 
 # Applica PyWal al primo sfondo selezionato
 wal -i "$1"
 
 # Genero il file color per Hyprland
-update_hyprland.sh
+$HOME/.config/style/update_hyprland.sh
 
 # Modifico il file style di SwayNC
-update_swaync.sh
+$HOME/.config/style/update_swaync.sh
+
+# Rimuovi lo sfondo ativo
+pkill hyprpaper
+
+# Attendi che lo sfondo sia effettivamente stato eliminato
+while pgrep hyprpaper > /dev/null; do
+    sleep 0.1
+done
 
 # Ricarica Hyprland, Hyprpaper, Waybar e swaync
-pkill hyprpaper && hyprpaper &
+hyprpaper &
 hyprctl reload
 pkill waybar && waybar &
 swaync-client -rs
