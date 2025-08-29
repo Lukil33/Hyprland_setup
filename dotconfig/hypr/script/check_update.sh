7f1aa2059fb382c7c2e781e90cc4c9613ad657b3
Lukil <prigioneluca33@gmail.com>
2025-08-29 11:12:35 +0200

    fix: :bug: migliorato lo spegnimento per inattività

---
+++ dotconfig/hypr/script/check_update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PROCESSI=("dnf" "dnf5" "rpm" "packagekitd" "pkcon" "flatpak" "snap" "pip3")
+LOCK_FILE="/var/cache/dnf/metadata_lock.pid"
+
+ACTIVE=0
+
+# Controllo processi attivi
+for proc in "${PROCESSI[@]}"; do
+    if pgrep -x "$proc" > /dev/null; then
+        ACTIVE=1
+    fi
+done
+
+# Controllo file di lock
+if [ -f "$LOCK_FILE" ]; then
+    ACTIVE=1
+fi
+
+if [ "$ACTIVE" = "1" ]; then
+    hyprctl dispatch idleinhibit always
+else
+    hyprctl dispatch idleinhibit none
+fi
